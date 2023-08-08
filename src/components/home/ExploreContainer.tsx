100136d280464058fc592ac1bcdf29024cfd21c8
Elisabeth Shakespeare <shakespearee04@gmail.com>
2023-08-08 15:10:32 -0500

    redo some of the styling, add a little more content, and use a fake image for the top of the page

--- src/components/home/ExploreContainer.tsx
+++ src/components/home/ExploreContainer.tsx
@@ -1,46 +1,47 @@
-import { IonButton, IonCard, IonCardContent, IonCardTitle, IonCol, IonGrid, IonItem, IonLabel, IonList, IonListHeader, IonRow, IonSearchbar, IonText } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonRow, IonSearchbar, IonText } from '@ionic/react';
 import './ExploreContainer.css';
-import { about } from '../strings';
+import { about, companyShortDesc, companySlogan, projects } from '../strings';
 
 interface ContainerProps { }
 
 const MainContainer: React.FC<ContainerProps> = () => {
   return (
-    <div id="container">
       <IonGrid>
         <IonRow><IonCol offset='10'><IonSearchbar/></IonCol></IonRow>
+        <div id='centered'><IonRow>
+          <IonCol><IonText color="primary"><img src="https://fakeimg.pl/1050x200/?text=BiblioNexus" /></IonText></IonCol>
+        </IonRow></div>
+        <div id='centered'>
         <IonRow>
-          <IonCol><IonText color="primary"><h1>We are BiblioNexus!</h1></IonText></IonCol>
+          <IonCol style={{ padding: 0}}><IonText color="medium"><h1>{companySlogan}</h1></IonText></IonCol>
         </IonRow>
+        <IonRow>
+          <IonCol style={{ padding: 0 }}><p>{companyShortDesc}</p></IonCol>
+        </IonRow>
+        <hr style={{ height: '1px', width: '50%', background: "black" }} />
         <IonRow>
           <IonCol offset='2'><IonButton>Our Projects</IonButton></IonCol>
           <IonCol pull='2'><IonButton>Contribute</IonButton></IonCol>
         </IonRow>
+        </div>
         <IonRow>
           <IonCol>
-            <IonCard>
-              <IonCardTitle><IonText color="secondary"><h2>About Us</h2></IonText></IonCardTitle>
+            <IonCard> 
+              <IonCardHeader><IonCardTitle><IonText color="secondary"><h2>About the Foundation</h2></IonText></IonCardTitle></IonCardHeader>
               <IonCardContent>{about}</IonCardContent>            
             </IonCard>
           </IonCol>
           <IonCol>
             <IonCard>
-              <IonCardTitle><IonText color="secondary"><h2>Most Recent...</h2></IonText></IonCardTitle>
+              <IonCardHeader><IonCardTitle><IonText color="secondary"><h2>Most Recent...</h2></IonText></IonCardTitle></IonCardHeader>
               <IonCardContent>
                 <IonList>
                   <IonListHeader><IonButton>See All</IonButton></IonListHeader>
-                  <IonItem>
-                    <IonLabel>Cool Project</IonLabel>
-                  </IonItem>
-                  <IonItem>
-                    <IonLabel>Cooler Project</IonLabel>
-                  </IonItem>
-                  <IonItem>
-                    <IonLabel>Even Cooler</IonLabel>
-                  </IonItem>
-                  <IonItem>
-                    <IonLabel>Coolest of all</IonLabel>
-                  </IonItem>
+                  {projects.map((project) => (
+                    <IonItem key={project.name}>
+                      <IonLabel><h2>{project.name}</h2><IonIcon icon="code-slash-outline" /><p>{project.description}</p></IonLabel>
+                    </IonItem>
+                  ))}
                 </IonList>
               </IonCardContent>
             </IonCard>
@@ -57,7 +58,6 @@ const MainContainer: React.FC<ContainerProps> = () => {
           <IonCol><IonCard><IonCardTitle>non consectetur a erat nam</IonCardTitle><IonCardContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed augue lacus viverra vitae congue eu consequat.</IonCardContent></IonCard></IonCol>
         </IonRow>
       </IonGrid>
-    </div>
   );
 };
 
