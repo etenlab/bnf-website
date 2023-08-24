9184e91d90d2c2362ff74f6cdbd082e6679e68de
Michael Marshall <michaelmarshall220@gmail.com>
2023-08-24 15:33:28 -0500

    tweaks

--- src/components/home/ExploreContainer.tsx
+++ src/components/home/ExploreContainer.tsx
@@ -1,66 +1,110 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonIcon, IonImg, IonItem, IonLabel, IonList, IonListHeader, IonRow, IonSearchbar, IonText } from '@ionic/react';
-import './ExploreContainer.css';
-import { about, companyShortDesc, companySlogan, projects } from '../strings';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonRow,
+  IonSearchbar,
+  IonText,
+} from "@ionic/react";
+import "./ExploreContainer.css";
+import { about, companyShortDesc, companySlogan, projects } from "../strings";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const MainContainer: React.FC<ContainerProps> = () => {
   return (
-      <IonGrid style={{ padding: "100px" }}>
-
-        <div className='title-image'>
-
+    <IonGrid style={{ padding: "100px" }}>
+      <div className="title-image">
         <IonImg src="./FullLogo_Transparent_NoBuffer.png"></IonImg>
-        </div>
+      </div>
 
-        <div id='centered'>
+      <div id="centered">
         <IonRow>
-          <IonCol style={{ padding: 0}}><IonText color="medium"><h1>{companySlogan}</h1></IonText></IonCol>
-        </IonRow>
-        <IonRow>
-          {/* <IonCol style={{ padding: 0 }}><p>{companyShortDesc}</p></IonCol> */}
-        </IonRow>
-        {/* <hr style={{ height: '1px', width: '50%', background: "black" }} /> */}
-        <IonRow>
-          {/* <IonCol offset='2'><IonButton>Our Projects</IonButton></IonCol>
-          <IonCol pull='2'><IonButton>Contribute</IonButton></IonCol> */}
-        </IonRow>
-        </div>
-        <IonRow>
-          <IonCol>
-            <IonCard id='ion-card'> 
-              <IonCardHeader><IonCardTitle><IonText color="secondary"><h2>About the Foundation</h2></IonText></IonCardTitle></IonCardHeader>
-              <IonCardContent>{about}</IonCardContent>            
-            </IonCard>
+          <IonCol style={{ padding: 0 }}>
+            <IonText color="medium">
+              <h1>{companySlogan}</h1>
+            </IonText>
           </IonCol>
-          <IonCol>
-            <IonCard id='ion-card'>
-              <IonCardHeader><IonCardTitle><IonText color="secondary"><h2>Our Projects</h2></IonText></IonCardTitle></IonCardHeader>
-              <IonCardContent>
-                <IonList>
-                  {projects.map((project) => (
-                    <IonItem key={project.name}>
-                      <IonLabel><h2>{project.name}</h2><IonIcon icon="code-slash-outline" /><p>{project.description}</p></IonLabel>
-                    </IonItem>
-                  ))}
-                </IonList>
-              </IonCardContent>
-            </IonCard>
+        </IonRow>
+      </div>
+      <IonRow>
+        <IonCol>
+          <IonCard id="ion-card">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonText color="secondary">
+                  <h2>About the Foundation</h2>
+                </IonText>
+              </IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>{about}</IonCardContent>
+          </IonCard>
+        </IonCol>
+        <IonCol>
+          <IonCard id="ion-card">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonText color="secondary">
+                  <h2>Our Projects</h2>
+                </IonText>
+              </IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonList>
+                {projects.map((project) => (
+                  <IonItem key={project.name}>
+                    <IonLabel>
+                      <h2>{project.name}</h2>
+                      <IonIcon icon="code-slash-outline" />
+                      <p>{project.description}</p>
+                    </IonLabel>
+                  </IonItem>
+                ))}
+              </IonList>
+            </IonCardContent>
+          </IonCard>
+        </IonCol>
+      </IonRow>
+
+      <div id="centered">
+        
+        <IonRow>
+          <IonCol style={{ padding: 0 }}>
+            <p>{companyShortDesc}</p>
           </IonCol>
         </IonRow>
         {/* <IonRow>
+          <IonCol offset="2">
+            <IonButton>Our Projects</IonButton>
+          </IonCol>
+          <IonCol pull="2">
+            <IonButton>Contribute</IonButton>
+          </IonCol>
+        </IonRow> */}
+      </div>
+
+      {/* <IonRow>
           <IonText color="primary" ><h1>News</h1></IonText>
         </IonRow> */}
-        {/* <IonRow>
+      {/* <IonRow>
           <IonCol><IonCard><IonCardTitle>non consectetur a erat nam</IonCardTitle><IonCardContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed augue lacus viverra vitae congue eu consequat.</IonCardContent></IonCard></IonCol>
           <IonCol><IonCard><IonCardTitle>non consectetur a erat nam</IonCardTitle><IonCardContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed augue lacus viverra vitae congue eu consequat.</IonCardContent></IonCard></IonCol>
           <IonCol><IonCard><IonCardTitle>non consectetur a erat nam</IonCardTitle><IonCardContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed augue lacus viverra vitae congue eu consequat.</IonCardContent></IonCard></IonCol>
           <IonCol><IonCard><IonCardTitle>non consectetur a erat nam</IonCardTitle><IonCardContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed augue lacus viverra vitae congue eu consequat.</IonCardContent></IonCard></IonCol>
           <IonCol><IonCard><IonCardTitle>non consectetur a erat nam</IonCardTitle><IonCardContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed augue lacus viverra vitae congue eu consequat.</IonCardContent></IonCard></IonCol>
         </IonRow> */}
-      </IonGrid>
+    </IonGrid>
   );
 };
-
 
 export default MainContainer;
--- src/components/strings.ts
+++ src/components/strings.ts
@@ -1,7 +1,7 @@
 export const companySlogan =
   "BiblioNexus Foundation serves the Bible translation movement by facilitating inter-organizational software development";
 export const companyShortDesc =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  "Get in touch using development at biblionexus dot org";
 export const about = `As it became apparent to the ETEN steering commitee that progress towards the All Access Goals (AAGs) was not on track to be met by 2033, an initiative was created to research and develop methods to accelerate Bible translation. One result of that effort was the creation of the BiblioNexus Foundation (BNF). The BNF is a not-for-profit organization the seeks to help Bible translation organizations collaborate more effectively within the software development lifecycle. The BNF is not a funding agency and does not make funding recommendations. The BNF is structured so that organizations can move projects under the BNF brand to help engender wider collaboration within the Bible translation movement and open source community.
 `;
 
@@ -20,7 +20,7 @@ export const projects = [
     demoLink: "",
     contact: "development@biblionexus.org",
     description:
-      "crowdrocks is a sandbox that experiments with crowdsourcing different parts of the Bible translation workflow",
+      "crowdrocks is a sandbox app that experiments with crowdsourcing different parts of the Bible translation workflow",
   },
   {
     name: "QA.Bible",
@@ -28,6 +28,6 @@ export const projects = [
     demoLink: "",
     contact: "nothing@nobody.com",
     description:
-      "QA.Bible is development new and innovative techniques to deliver high-quality translations at a faster rate",
+      "QA.Bible is developing new and innovative techniques to deliver high-quality translations at a faster rate",
   },
 ];
